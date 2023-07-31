ab62b9088e086e03d316ecab075270ca730a7ec5
Daniel <danielmtdc16@gmail.com>
2023-07-31 14:25:54 -0300

    adicionando botões que faltaram.

---
+++ script.js
@@ -0,0 +1,6 @@
+let minutes = document.querySelector(".minutes");
+let seconds = document.querySelector(".seconds");
+let milliseconds = document.querySelector(".milliseconds");
+const start_button = document.querySelector(".start-pause");
+const reset_button = document.querySelector(".reset");
+
